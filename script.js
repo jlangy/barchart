d64b9082f18f8e19d4e8c928f97169fe72895c25
Jon <jonathan-langlois@Live.ca>
2019-11-04 16:35:14 +0000

    makes bar heights based on data

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
+const chartHeight = 400 * 0.8;
+const PERCENT_HEIGHT_FILLED = 0.9;
 
-function loadData(){
-  const data = [3, 4, 2, 5];
+function loadData() {
+  const data = [3, 4, 2, 5, 20, 14, 3, 13, 2, 17.53, 25];
+  unitHeight = chartHeight * PERCENT_HEIGHT_FILLED / Math.max(...data);
   data.forEach(val => {
-    $('#chart').append("<div class='bar'></div>");
+    $('#chart').append("<div class='bar' style='height:" + unitHeight * val + "px'></div>");
   });
 }
 
