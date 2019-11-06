dca676585e2a827f055a4dd5c2cad095be7f959f
Jon <jonathan-langlois@Live.ca>
2019-11-06 18:15:28 +0000

    adds basic x axis labels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const chartHeight = 400 * 0.8;
 const PERCENT_HEIGHT_FILLED = 0.9;
-const data = [3, 4, 5, 7, 17.53, 2];
+const data = [300, 400, 900, 600, 1700, 200];
 const unitHeight = chartHeight * PERCENT_HEIGHT_FILLED / Math.max(...data);
 
 //Need to fiugre out how to calculate this based on input chart height and number of values
@@ -13,7 +13,7 @@ function loadData() {
   });
 }
 
-function makeLabels() {
+function makeYLabels() {
   //Currently assuming positive data
   const top = Math.max(...data) / PERCENT_HEIGHT_FILLED;
   const ySpacing = Math.round(top / Y_SPACING);
@@ -21,10 +21,15 @@ function makeLabels() {
     $('#y-axis').append('<p class="label" style="height:' + ySpacing * unitHeight + 'px">' + i + '-' + '</p>');
   }
 }
-
+function makeXLabels() {
+  data.forEach(val => {
+    $('#x-axis').append("<p class='xlabel'>" + val + "</p>");
+  });
+}
 function main() {
   loadData();
-  makeLabels();
+  makeYLabels();
+  makeXLabels();
 }
 
 $(document).ready(main());
