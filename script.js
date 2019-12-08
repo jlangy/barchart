3b0581b0a74883fe28a09c9eb7a2a3879dffb44d
Jon <jonathan-langlois@Live.ca>
2019-12-08 00:37:14 +0000

    begin reformat

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const PERCENT_HEIGHT_FILLED = 0.95;
-const data = [[4, 1], { label: 'hi', data: [7, 1, 2], colours: ['blue', 'white'], labelColours: ['white', 'green'] }, {data: [3], colours: ['white']}, 4, 5];
+const data = [[4, 1], { label: 'hi', data: [7, 1, 2], colours: ['blue', 'white'], labelColours: ['white', 'green'] }, { data: [3], colours: ['white'] }, 4, 5];
 const DEFAULT_COLOUR = 'red';
 const DEFAULT_LABEL_COLOUR = 'white';
 const options = {
@@ -16,20 +16,45 @@ const options = {
   axisTitleFontSize: '1.2em',
   labelColour: "red",
   fontSize: '1em',
-  yAxisTicks: [0,1,2,3,4,5,7,8,9,10]
+  yAxisTicks: [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
 }
 
-function makeChart() {
-  $('#bar-chart').css({ 'height': String(options.height), 'width': String(options.width) })
-  const chartWidth = Number(options.width.match(/\d*\.*\d*/)[0]) * 0.9;
-  const barWidth = chartWidth * (1 - options.barSpacing) / data.length;
-  const unitHeight = getUnitHeight();
-  makeXLabels();
-  makeYLabels(unitHeight);
-  makeTitles();
-  loadData(unitHeight);
-  setBarStyles(barWidth);
-  setFontStyles();
+{/* <div id="bar-chart">
+    <h2 id="chart-title">Chart Title</h2>
+    <div id='y-axis-title-container'>
+      <div>
+        <div id='y-axis-title'>y axis title</div>
+      </div>
+    </div>
+    <div class='axis' id="y-axis"></div>
+    <div id="chart">
+    </div>
+    <div class='axis' id="x-axis"></div>
+    <div id="x-axis-title">x axis title</div>
+  </div>` */}
+
+function makeChart(element) {
+  let $barChart = $('<div/>').appendTo(element).css({
+    'height': String(options.height),
+    'width': String(options.width),
+    'height': '400px',
+    'width': '600px',
+    'background': 'rgb(226, 123, 226)',
+    'border': 'black 1px solid',
+    'display': 'grid',
+    'grid-template-columns': '5% 5% 90%',
+    'grid-template-rows': '10% 70% 10% 10%',
+    'margin': 'auto'
+  });
+  makeTitles($barChart);
+  // const chartWidth = Number(options.width.match(/\d*\.*\d*/)[0]) * 0.9;
+  // const barWidth = chartWidth * (1 - options.barSpacing) / data.length;
+  // const unitHeight = getUnitHeight();
+  // makeXLabels();
+  // makeYLabels(unitHeight);
+  // loadData(unitHeight);
+  // setBarStyles(barWidth);
+  // setFontStyles();
 }
 
 function setFontStyles() {
@@ -129,18 +154,43 @@ function makeXLabels() {
   });
 }
 
-function makeTitles(){
+function makeTitles(barChart) {
   const title = options.title ? options.title : '';
   const xAxisTitle = options.xAxisTitle ? options.xAxisTitle : '';
   const yAxisTitle = options.yAxisTitle ? options.yAxisTitle : '';
-  $('#x-axis-title').text(xAxisTitle);
-  $('#y-axis-title').text(yAxisTitle);
-  $('#chart-title').text(title);
-  $('#chart-title').css({'font-size': options.titleFontSize, 'color': options.titleFontColour });
+  const xAxisElement = $('<div/>').appendTo(barChart).css({
+    'grid-column': '3 / 4',
+    'display': 'flex',
+    'align-items': 'center',
+    'justify-content': 'center',
+    'overflow': 'hidden',
+    'grid-row': '3'
+  }).text(xAxisTitle);
+  const yAxisElement = $('<div/>').appendTo(barChart).css({
+    'height': '1.5em',
+    'white-space': 'nowrap',
+    'margin-left': '-100%',
+    'text-align': 'center',
+    'transform': 'rotate(-90deg)',
+    'transform-origin': 'center',
+    'position': 'relative',
+    'left': '1em'
+  }).text(yAxisTitle);
+  const chartTitle = $('<div/>').appendTo(barChart).css({
+      'grid-column': 'span 3',
+      'grid-row': '1',
+      'box-sizing': 'border-box',
+      'margin': '0',
+      'display': 'flex',
+      'justify-content': 'space-around',
+      'overflow': 'hidden'
+  }).text(title);
+  // $('#chart-title').text(title);
+  // $('#chart-title').css({ 'font-size': options.titleFontSize, 'color': options.titleFontColour });
 }
 
 function main() {
-  makeChart();
+  makeChart($('#ex1'));
 }
 
 $(document).ready(main());
