b1d28da1e25bc53d1d6448bcb881ffa4bc95b8ec
Jon <jonathan-langlois@Live.ca>
2019-12-15 19:05:21 +0000

    update examples

--- makeExamples.js
+++ makeExamples.js
@@ -1,48 +1,12 @@
-const data1 = [
-  {
-    data: [7, 10, 6],
-    label: "Q1",
-    colours: ['black', 'white'],
-    labelColours: ['white', 'black']
-  },
-  {
-    data: [6,14, 3],
-    label: "Q2",
-    colours: ['black', 'white'],
-    labelColours: ['white', 'black']
-  },
-  {
-    data: [10, 12, 4],
-    label: "Q3",
-    colours: ['black', 'white'],
-    labelColours: ['white', 'black']
-  },
-  {
-    data: [3, 6, 3],
-    label: "Q4",
-    colours: ['black', 'white'],
-    labelColours: ['white', 'black']
-  }
-];
+const data1 = [3, 2, 5, 4, 3];
 const options1 = {
-  height: '500px',
-  width: '600px',
-  backgroundColour: 'wheat',
-  title: 'Sales: p1 (black) vs p2 (white) vs p3(red)',
-  titleFontColour: 'Red',
-  xAxisTitle: 'Quarter',
-  yAxisTitle: 'Thousands (CAD)',
-  titleFontSize: '1.5em',
-  barSpacing: 0.4,
-  axisTitleFontSize: '1.2em',
-  yAxisTicks: [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30],
-  barBorders: 'on',
-  dataPosition: 'top',
-  barColour: 'red',
-  labelColour: 'black',
-  dataPosition: 'top'
-}
-const data2 = [[3, 4], [1, 2, 1, 4], 12, [3, 2]];
+  height: '400px',
+  width: '500px',
+  title: 'Simple Chart'
+};
+
+
+const data2 = [[3, 4], [3, 7, 4], 12, [3, 2]];
 const options2 = {
   height: '400px',
   width: '500px',
@@ -53,24 +17,38 @@ const options2 = {
   yAxisTitle: 'Range',
   axisTitleFontSize: '1em',
   fontSize: '1em',
-  barColour: 'red'
-}
+  barColour: 'red',
+  labelColour: 'black'
+};
 
-const data3 = [{ data: [4, 3], colours: ['red', 'cyan'], labelColours: ['white', 'black'], label: 'A really really long x axis label that overflows' },
-{ data: [2, 1, 2, 1], colours: ['red', 'cyan', 'red', 'cyan'], labelColours: ['white', 'black', 'white', 'black'], label: '2020' }];
+const data3 = [{ data: [4, 3], colours: ['red', 'blue'], labelColours: ['black', 'white'], label: '2019' },
+{ data: [2, 1, 2, 1], colours: ['red', 'blue', 'red', 'blue'], labelColours: ['black', 'white', 'black', 'white'], label: '2020' }];
 
 const options3 = {
   height: '400px',
   width: '500px',
+  backgroundColour: 'white',
   title: "Stacked Coloured Chart",
-  titleFontSize: '2em',
+  titleFontSize: '1.5em',
   xAxisTitle: 'Domain',
   yAxisTitle: 'Range',
-  axisTitleFontSize: '1.3em',
-  fontSize: '1em',
+  axisTitleFontSize: '1.2em',
+  fontSize: '1.2em',
   yAxisTicks: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-  barSpacing: 0.5
+  barSpacing: 0.5,
+  dataPosition: 'top'
 };
+
+const data = [45, 37, 56, 32]
+
+const options = {
+  height: '400px',
+  width: '500px',
+  backgroundColour: 'white',
+  title: 'Sales',
+  xAxisTitle: 'Quarter',
+  yAxisTitle: 'Thousands(CAD)'
+}
 
 function main() {
   drawBarChart(data1, options1, document.getElementById('ex1'));
